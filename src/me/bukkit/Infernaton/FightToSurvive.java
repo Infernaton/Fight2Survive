1cbcdc5b61d86d2abf4950aefc0821e76ed552fb
Brian <nairb725@gmail.com>
2022-01-13 09:38:59 +0100

    spawn pnj and custom trade

--- src/me/bukkit/Infernaton/FightToSurvive.java
+++ src/me/bukkit/Infernaton/FightToSurvive.java
@@ -3,6 +3,14 @@ package me.bukkit.Infernaton;
 import me.bukkit.Infernaton.commands.DebugCommand;
 import me.bukkit.Infernaton.listeners.PlayerListeners;
 import org.bukkit.ChatColor;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+import org.bukkit.entity.Villager;
+import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -30,4 +38,39 @@ public class FightToSurvive extends JavaPlugin {
 
     @Override
     public void onDisable(){ }
+
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+
+
+        if(cmd.getName().equalsIgnoreCase("mob") && sender instanceof Player){
+
+            Player player = (Player)sender;
+            Location location = new Location(player.getWorld(), 85.976,56,177.573);
+
+            Villager villager = (Villager) location.getWorld().spawnEntity(location, EntityType.VILLAGER);
+            villager.setCustomName("Passe Partout");
+            villager.setCustomNameVisible(true);
+
+            return true;
+        }
+        if(cmd.getName().equalsIgnoreCase("hello") && sender instanceof Player){
+
+            Player player = (Player)sender;
+
+            player.sendMessage("Hello, "+ player.getName());
+
+            return true;
+        }
+
+        if(cmd.getName().equalsIgnoreCase("trade") && sender instanceof Player){
+            Player p = (Player)sender;
+            if(args.length == 0){
+                OpenMenuTrade trade = new OpenMenuTrade("shop");
+                trade.addTrade(new ItemStack(Material.COBBLESTONE, 10),new ItemStack(Material.WOOL, 30));
+                trade.addTrade(new ItemStack(Material.STICK, 2),new ItemStack(Material.WOOD, 3), new ItemStack(Material.WOOD_AXE, 1));
+                trade.openTrade(p);
+            }
+        }
+        return false;
+    }
 }
