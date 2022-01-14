f785ec601473d5defe22a435356c20f32595c9ac
Infernaton <lubin.pauly@gmail.com>
2022-01-14 18:29:22 +0100

    fix Clock when the player is in creative

--- src/me/bukkit/Infernaton/listeners/BlockListener.java
+++ src/me/bukkit/Infernaton/listeners/BlockListener.java
@@ -2,7 +2,9 @@ package me.bukkit.Infernaton.listeners;
 
 import me.bukkit.Infernaton.FightToSurvive;
 import me.bukkit.Infernaton.builder.Clock;
+import org.bukkit.GameMode;
 import org.bukkit.block.Block;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.block.BlockBreakEvent;
@@ -17,7 +19,10 @@ public class BlockListener implements Listener {
 
     @EventHandler
     public void blockBreak(BlockBreakEvent event){
-        Block block = event.getBlock();
-        Clock.newCountDown(main, 10, block);
+        Player player = event.getPlayer();
+        if (player.getGameMode() != GameMode.CREATIVE){
+            Block block = event.getBlock();
+            Clock.newCountDown(main, 10, block);
+        }
     }
 }
