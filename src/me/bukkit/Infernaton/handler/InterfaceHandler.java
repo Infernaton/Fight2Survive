5c252316c9116dcef87b4a61e9ee85aed0f70670
Infernaton <lubin.pauly@gmail.com>
2022-06-01 21:29:40 +0200

    Fix separator + incorrect usage of Color Chat

--- src/me/bukkit/Infernaton/handler/InterfaceHandler.java
+++ src/me/bukkit/Infernaton/handler/InterfaceHandler.java
@@ -6,6 +6,10 @@ import org.bukkit.Bukkit;
 import org.bukkit.inventory.Inventory;
 import org.bukkit.inventory.ItemStack;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class InterfaceHandler {
 
     private FightToSurvive main;
@@ -14,21 +18,19 @@ public class InterfaceHandler {
         this.main = main;
     }
 
-    public Inventory selectTeam(){
-        Inventory inv = Bukkit.createInventory(null, 45, main.stringH().teamInventory());
-        inv.setItem(20, main.HI().blueWool());
-        inv.setItem(24, main.HI().redWool());
-        inv.setItem(13, main.HI().spectatorWool());
-        inv.setItem(40, main.HI().gameStartWool());
-        inv.setItem(44, main.HI().optionsWool());
-
-        return inv;
+    private Map<Integer, ItemStack> createException(int pos, ItemStack item){
+        Map<Integer, ItemStack> except = new HashMap<>();
+        except.put(pos, item);
+        return except;
     }
-    public Inventory cancelStart(){
-        Inventory inv = Bukkit.createInventory(null, 9, main.stringH().cancelInventory());
-        for (int i=0; i <inv.getSize(); i++){
-            if (i==4){
-                inv.setItem(i, main.HI().gameCancelWool());
+    private Map<Integer, ItemStack> createException(Map<Integer, ItemStack> except, int pos, ItemStack item){
+        except.put(pos, item);
+        return except;
+    }
+    private Inventory separatorLine(Inventory inv, int firstCell, Map<Integer, ItemStack> except){
+        for(int i=firstCell; i< firstCell+9; i++){
+            if (except.containsKey(i)){
+                inv.setItem(i, except.get(i));
             }else {
                 inv.setItem(i, main.HI().separator());
             }
@@ -36,9 +38,26 @@ public class InterfaceHandler {
         return inv;
     }
 
+    public Inventory selectTeam(){
+        Inventory inv = Bukkit.createInventory(null, 45, main.stringH().teamInventory());
+        inv.setItem(20, main.HI().blueWool());
+        inv.setItem(24, main.HI().redWool());
+        inv.setItem(13, main.HI().spectatorWool());
+
+        separatorLine(inv, 36, createException(createException(40, main.HI().gameStartWool()), 44, main.HI().optionsWool()));
+
+        return inv;
+    }
+    public Inventory cancelStart(){
+        Inventory inv = Bukkit.createInventory(null, 9, main.stringH().cancelInventory());
+        separatorLine(inv, 0, createException(4, main.HI().gameCancelWool()));
+        return inv;
+    }
+
     public Inventory optionsInventory(){
         Inventory inv = Bukkit.createInventory(null, 45, main.stringH().optionInventory());
-        inv.setItem(44, main.HI().returnWool());
+
+        separatorLine(inv, 36, createException(44, main.HI().returnWool()));
         return inv;
     }
 }
